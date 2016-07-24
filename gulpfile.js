8343377025c4bd3b34c887b76b16b053ec522417
Michael Kornblum <mkornblum71@gmail.com>
2016-07-24 11:24:04 -0500

    add file-include task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var $g = require('gulp-load-plugins')();
 
-gulp.task('include', function() {
+gulp.task('include', function () {
   gulp.src('./*.html')
     .pipe($g.fileInclude({
-
+      prefix: '@@',
+      basepath: '@file',
     }))
-})
+    .pipe(gulp.dest('./build'));
+});
