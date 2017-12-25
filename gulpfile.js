eabd04ece389e7b48705c9a759ab22daab75d9b5
michaelkornblum <mkornblum71@gmail.com>
2017-12-25 03:21:20 -0600

    remove cache

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,6 @@ const { src, dest } = gulp;
 
 gulp.task('pages', () =>
   src('./pages/*.html')
-    .pipe($g.cached('including'))
     .pipe($g.plumber())
     .pipe($g.fileInclude('@@'))
     .pipe($g.htmlmin({
@@ -59,14 +58,12 @@ gulp.task('styles', () =>
 
 gulp.task('images', () =>
   src('./images/*')
-    .pipe($g.cached('optimizing'))
     .pipe($g.plumber())
     .pipe($g.imagemin())
     .pipe(dest('./build/images')));
 
 gulp.task('vectors', () =>
   src('./vectors/*')
-    .pipe($g.cache('inlining'))
     .pipe($g.plumber())
     .pipe($g.svgmin())
     .pipe(dest('./pages/partials')));
